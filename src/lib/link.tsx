b15de7de64786a5b7e16c3c1d4bece5c02aa62f6
Gadi Cohen <dragon@wastelands.net>
2024-01-18 12:32:35 +0200

    link: use only usePathname, don't need useRouter

--- src/lib/link.tsx
+++ src/lib/link.tsx
@@ -1,7 +1,8 @@
 "use client";
 import * as React from "react";
 import clsx from "clsx";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import NextLink, { LinkProps as NextLinkProps } from "next/link";
 import MuiLink, { LinkProps as MuiLinkProps } from "@mui/material/Link";
 import { styled } from "@mui/material/styles";
@@ -72,10 +73,12 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
       ...other
     } = props;
 
-    const router = useRouter();
-    const pathname = typeof href === "string" ? href : href.pathname;
+    // const router = useRouter();
+    const browserPathname = typeof href === "string" ? href : href.pathname;
+    const routerPathname = usePathname(); // router.pathname
+
     const className = clsx(classNameProps, {
-      [activeClassName]: router.pathname === pathname && activeClassName,
+      [activeClassName]: routerPathname === browserPathname && activeClassName,
     });
 
     const isExternal =
