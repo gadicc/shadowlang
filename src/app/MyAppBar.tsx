f4a69583b0c2ac963303e44c241600d7168c852b
Gadi Cohen <dragon@wastelands.net>
2024-01-18 11:45:17 +0200

    app drawer, link

--- src/app/MyAppBar.tsx
+++ src/app/MyAppBar.tsx
@@ -3,105 +3,206 @@ import * as React from "react";
 import { useGongoOne, useGongoUserId } from "gongo-client-react";
 import { signIn } from "next-auth/react";
 
+import Link from "@/lib/link";
+import { AccountCircle } from "@mui/icons-material";
+
 import {
   AppBar,
   Avatar,
   Box,
   Button,
+  Divider,
   Toolbar,
   Typography,
   IconButton,
   Switch,
+  SwipeableDrawer,
   FormControlLabel,
   FormGroup,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
   MenuItem,
   Menu,
 } from "@mui/material";
-import { Menu as MenuIcon, AccountCircle } from "@mui/icons-material";
+import {
+  AdminPanelSettings,
+  Home,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
 
 export default function MyAppBar() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const userId = useGongoUserId();
   const user = useGongoOne((db) =>
     db.collection("users").find({ _id: userId }),
   );
+  const isAdmin = user && user.admin;
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+  const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const handleUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleUserClose = () => {
     setAnchorEl(null);
   };
 
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Photos
+  const toggleDrawer = React.useCallback(
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setMobileOpen(open);
+    },
+    [],
+  );
+
+  const drawer = React.useMemo(
+    () => (
+      <Box
+        role="presentation"
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
+      >
+        <Typography variant="h6" sx={{ my: 2, ml: 4 }}>
+          ShadowLang
         </Typography>
-        {userId ? (
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <Avatar
-                alt={
-                  typeof user?.displayName === "string"
-                    ? user.displayName
-                    : "avatar"
-                }
-                src={
-                  /* @ts-expect-error: TODO */
-                  user.photos[0].value
-                }
-                imgProps={{ referrerPolicy: "no-referrer" }}
-              />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleClose}>My account</MenuItem>
-            </Menu>
-          </div>
-        ) : (
-          <Button
-            variant="text"
-            sx={{ color: "white" }}
-            onClick={() => signIn()}
+
+        <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} href="/">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText>Home</ListItemText>
+            </ListItemButton>
+          </ListItem>
+
+          {isAdmin ? (
+            <ListItem disablePadding>
+              <ListItemButton component={Link} href="/admin">
+                <ListItemIcon>
+                  <AdminPanelSettings />
+                </ListItemIcon>
+                <ListItemText>Admin</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ) : undefined}
+        </List>
+      </Box>
+    ),
+    [isAdmin, toggleDrawer],
+  );
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
           >
-            LOGIN
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Photos
+          </Typography>
+          {userId ? (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleUserMenu}
+                color="inherit"
+              >
+                {user?.image ? (
+                  <Avatar
+                    alt={
+                      typeof user?.displayName === "string"
+                        ? user.displayName
+                        : "avatar"
+                    }
+                    src={user?.image as string}
+                    imgProps={{ referrerPolicy: "no-referrer" }}
+                  />
+                ) : (
+                  <AccountCircle />
+                )}
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleUserClose}
+              >
+                <MenuItem onClick={handleUserClose}>Profile</MenuItem>
+                <MenuItem onClick={handleUserClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <Button
+              variant="text"
+              sx={{ color: "white" }}
+              onClick={() => signIn()}
+            >
+              LOGIN
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <SwipeableDrawer
+          swipeAreaWidth={15}
+          hysteresis={0.3}
+          disableBackdropTransition={!iOS}
+          disableDiscovery={iOS}
+          open={mobileOpen}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            // display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: 260,
+            },
+          }}
+        >
+          {drawer}
+        </SwipeableDrawer>
+      </Box>
+    </>
   );
 }
