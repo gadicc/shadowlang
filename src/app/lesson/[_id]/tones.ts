0b5db50ff2a8f158f7d0e5abdb53efccad1797a9
Gadi Cohen <dragon@wastelands.net>
2024-03-07 12:04:51 +0000

    tones: use real audio files

--- src/app/lesson/[_id]/tones.ts
+++ src/app/lesson/[_id]/tones.ts
@@ -1,61 +1,22 @@
 "use client";
 
-// don't import before user interaction to avoid audiocontext warnings.
-// https://github.com/Tonejs/Tone.js/issues/443
-// https://github.com/Tonejs/Tone.js/issues/1102
-//import * as Tone from "tone";
-import type * as ToneType from "tone";
+const path = "/audio/";
 
-let Tone: typeof ToneType | null = null;
-let synth: ToneType.Synth<ToneType.SynthOptions> | null = null;
-let toneNow: ReturnType<typeof ToneType.now> | null = null; // Tone.now();
+const files = {
+  listen: "131658__bertrof__game-sound-selection.wav",
+  correct: "131662__bertrof__game-sound-correct_v2.wav",
+  incorrect: "351563__bertrof__game-sound-incorrect-with-delay.wav",
+};
+
+const audios = {
+  listen: new Audio(path + files.listen),
+  correct: new Audio(path + files.correct),
+  incorrect: new Audio(path + files.incorrect),
+};
 
 export async function playTone(tune: "listen" | "correct" | "incorrect") {
-  if (!(Tone && toneNow && synth)) return;
-  const now = Tone.now();
-  if (toneNow < now) toneNow = now;
-
-  const context = Tone.getContext();
-  if (context.state !== "running" && context.state !== "closed")
-    await context.resume();
-
-  // console.log("playTone", tune);
-  switch (tune) {
-    case "listen":
-      synth.triggerAttackRelease("D5", "16n", toneNow);
-      break;
-    case "correct":
-      synth.triggerAttackRelease("C5", "16n", toneNow);
-      synth.triggerAttackRelease("G5", "16n", (toneNow += 0.25));
-      toneNow += 0.25;
-      break;
-    case "incorrect":
-      synth.triggerAttackRelease("G5", "16n", toneNow);
-      synth.triggerAttackRelease("C5", "16n", (toneNow += 0.25));
-      toneNow += 0.25;
-      break;
-    default:
-      throw new Error("unknown tune: " + tune);
-  }
-}
-
-let setupHasRun = false;
-async function setup() {
-  if (synth || setupHasRun) return;
-  setupHasRun = true;
-  document.removeEventListener("touchstart", setup);
-  document.removeEventListener("touchend", setup);
-  document.removeEventListener("click", setup);
-  document.removeEventListener("keydown", setup);
-  Tone = await import("tone");
-  await Tone.start();
-  synth = new Tone.Synth().toDestination();
-  toneNow = Tone.now();
-}
-
-if (typeof document === "object") {
-  document.addEventListener("touchstart", setup);
-  document.addEventListener("touchend", setup);
-  document.addEventListener("click", setup);
-  document.addEventListener("keydown", setup);
+  const audio = audios[tune];
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
 }
